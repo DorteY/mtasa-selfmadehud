4b4bd30ff5d4d9176f6c245adbf69ae66bcbdfbd
Dennis <huarycane@gmail.com>
2019-06-13 11:34:14 +0200

    bug fix

--- HUD.lua
+++ HUD.lua
@@ -26,13 +26,13 @@ function SelfmadeHUD()
 	local health = math.floor (getElementHealth (lp))
 	dxDrawText("Health • "..health.." %",3450*sx,32*sy,100*sx,15*sy,tocolor(255,255,255,255),1.50,"default-bold","center",_,_,_,_,_,_)
 	
-	if not isElementInWater(lp) then
+	if not isElementInWater(lp)then
 		dxDrawRectangle(1600*sx,80*sy,300*sx,50*sy,tocolor(0,0,0,120),false)--ARMOR Bar
 		dxDrawRectangle(1660*sx,84*sy,armor*sx,42*sy,tocolor(0,200,200,200),true)--ARMOR Progress
 		dxDrawImage(1605*sx,82*sy,47*sx,45*sy,"images/ARMOR.png",0,0,0,tocolor(255, 255, 255, 255))
 		local armor=math.floor (getPedArmor(lp))
-		dxDrawText("Armor • "..armor.." %", 3450*sx,92*sy,100*sx,15*sy,tocolor(255, 255, 255, 255), 1.50,"default-bold","center",_,_,_,_,_,_)
-	elseif isElementInWater(lp) then
+		dxDrawText("Armor • "..armor.." %", 3450*sx,92*sy,100*sx,15*sy,tocolor(255, 255, 255, 255),1.50,"default-bold","center",_,_,_,_,_,_)
+	elseif isElementInWater(lp)then
 		dxDrawRectangle(1600*sx,80*sy,300*sx,50*sy,tocolor(0,0,0,120),false)--OXYGEN Bar
 		dxDrawRectangle(1660*sx,84*sy,572/2430*oxygen,42*sy,tocolor(0,200,200,200),true)--OXYGEN Progress
 		dxDrawImage(1605*sx,82*sy,47*sx,45*sy,"images/OXYGEN.png",0,0,0,tocolor(255,255,255,255))
@@ -48,7 +48,7 @@ function SelfmadeHUD()
 	local weaponID = getPedWeapon (lp)
 	dxDrawImage(1460*sx, 20*sy, 100*sx, 100*sy,"images/WEAPONS/"..tostring(weaponID)..".png",0.0,0.0,0.0,tocolor(255,255,255,200),false)
 	if getPedTotalAmmo(lp)>1 then
-	dxDrawText(getPedAmmoInClip(lp).." | "..getPedTotalAmmo(lp)-getPedAmmoInClip(lp),2920*sx, 120*sy, 100*sx, 15*sy, tocolor(255, 255, 225, 255), 1.30, "default-bold", "center", "top", false, false, true, true, false)
+		dxDrawText(getPedAmmoInClip(lp).." | "..getPedTotalAmmo(lp)-getPedAmmoInClip(lp),2920*sx,120*sy,100*sx,15*sy,tocolor(255,255,225,255),1.30,"default-bold","center",_,_,_,_,_,_)
 	end
 
 end
